bd2ea747a1c7839a77d3370b6efc0bb39e08ca66
rhyme884 <rhyme884@gmail.com>
2021-03-01 18:52:22 +0900

    user management apis developed

--- src/spring/graphql/src/main/java/r/demo/graphql/domain/user/UserInfoRepo.java
+++ src/spring/graphql/src/main/java/r/demo/graphql/domain/user/UserInfoRepo.java
@@ -1,10 +1,19 @@
 package r.demo.graphql.domain.user;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserInfoRepo extends CrudRepository<UserInfo, Long> {
     Boolean existsByEmail(String email);
     Optional<UserInfo> findByEmail(String email);
+    Page<UserInfo> findAll(Pageable pageable);
+    Page<UserInfo> findAllByAuthorityIsIn(List<String> authority, Pageable pageable);
+
+    @Query(value = "select * from user u where u.authority = ?1 order by u.id limit 1", nativeQuery = true)
+    Optional<UserInfo> getFirstUserOfAuthority(String authority);
 }
