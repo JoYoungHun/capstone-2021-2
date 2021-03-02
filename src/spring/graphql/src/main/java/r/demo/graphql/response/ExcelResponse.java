23576756f4f8c26bb01d73ec77e70aabfdee5888
rhyme884 <rhyme884@gmail.com>
2021-03-02 20:02:40 +0900

    excel upload/download apis developed

--- src/spring/graphql/src/main/java/r/demo/graphql/response/ExcelResponse.java
+++ src/spring/graphql/src/main/java/r/demo/graphql/response/ExcelResponse.java
@@ -1,2 +1,23 @@
-package r.demo.graphql.response;public class ExcelResponse {
+package r.demo.graphql.response;
+
+import lombok.Getter;
+import r.demo.graphql.types.Paragraph;
+
+import java.util.Collections;
+import java.util.List;
+
+@Getter
+public class ExcelResponse {
+    private final int status;
+    private final List<Paragraph> paragraphs;
+
+    public ExcelResponse(int status) {
+        this.status = status;
+        this.paragraphs = Collections.emptyList();
+    }
+
+    public ExcelResponse(int status, List<Paragraph> paragraphs) {
+        this.status = status;
+        this.paragraphs = paragraphs;
+    }
 }
